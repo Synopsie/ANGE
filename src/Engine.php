f6fe2988423c4bd093159c769361ec8789312e3e
Dumont-Julien <dumontj357@gmail.com>
2024-07-21 12:00:57 +0200

    Base

---
+++ src/Engine.php
@@ -0,0 +1,16 @@
+<?php
+declare(strict_types=1);
+
+namespace synopsie;
+
+use pocketmine\plugin\PluginBase;
+
+class Engine extends PluginBase {
+
+    protected function onLoad() : void {
+
+    }
+
+
+
+}
